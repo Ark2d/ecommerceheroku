f3083f5bcec00a8d918e0bb9c10a5021a04a30b9
Vinicius Almeida <v.almeida1284@gmail.com>
2020-05-14 13:06:36 -0400

    Tela Usuarios / Alteração de Dados e Senha

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     # ---------------------------------------------- Base Routes -------------------------------------------------------
+    url(r'^$', views.index, name='index'),
     url(r'^regostro/$', views.register, name='register'),
+    url(r'^alterar-dados/$', views.update_user, name='update_user'),
+    url(r'^alterar-senha/$', views.update_password, name='update_password'),
 ]
